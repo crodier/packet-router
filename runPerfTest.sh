c170ea646a990c6c9173a58c1c249af02db03286
Chris Rodier <chrisrodier@gmail.com>
2016-05-29 21:42:30 -0400

    faster withot G1GC

--- runPerfTest.sh
+++ runPerfTest.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 mvn package
-java -server -XX:+UseG1GC -XX:MaxGCPauseMillis=2 -jar target/packet-router-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+echo without G1GC
+java -jar target/packet-router-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+#echo with G1GC
+#java -server -XX:+UseG1GC -XX:MaxGCPauseMillis=2 -jar target/packet-router-1.0-SNAPSHOT-jar-with-dependencies.jar
