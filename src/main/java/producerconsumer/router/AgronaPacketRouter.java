1a0fdeae5ffdd2a4dcdab524375962b9d9b14f25
Chris Rodier <chrisrodier@gmail.com>
2016-05-29 21:31:46 -0400

    Add Agrona and test cases Without G1GC appears to be the best!

---
+++ src/main/java/producerconsumer/router/AgronaPacketRouter.java
@@ -0,0 +1,70 @@
+package producerconsumer.router;
+
+
+import org.agrona.concurrent.ManyToManyConcurrentArrayQueue;
+import org.agrona.concurrent.ManyToOneConcurrentArrayQueue;
+import org.agrona.concurrent.OneToOneConcurrentArrayQueue;
+import producerconsumer.router.wait.BlockingWaitStrategy;
+import producerconsumer.router.wait.BusyWaitStrategy;
+import producerconsumer.router.wait.WaitStrategy;
+
+import java.util.Queue;
+
+/**
+ * https://github.com/real-logic/Agrona
+ *
+ * Like JCTools, another crack at Leslie Lamport Queues
+ *
+ * From Martin Thomson, finance.
+ */
+public class AgronaPacketRouter extends QueuePacketRouter implements PacketRouter {
+
+    public enum QueueType {
+        SPSC,
+        MPSC,
+        MPMC
+    }
+
+    BusyWaitStrategy busyWaitStrategy = new BusyWaitStrategy(this);
+    BlockingWaitStrategy blockingWaitStrategy = new BlockingWaitStrategy(this);
+
+    boolean isBlocking = false;
+
+    public AgronaPacketRouter(boolean blocking, QueueType queueType) {
+        super(new JCToolsFactory(queueType));
+        isBlocking = blocking;
+    }
+
+    @Override
+    public WaitStrategy getWaitStrategy() {
+        if (isBlocking)
+            return blockingWaitStrategy;
+        else
+            return busyWaitStrategy;
+    }
+
+    /**
+     * Only works for single producer, single consumer!
+     */
+    public static class JCToolsFactory implements QueueFactory {
+        private final QueueType queue;
+
+        int POWER_OF_TWO = 22; // 8 million
+
+        public JCToolsFactory(QueueType qt) {
+            this.queue = qt;
+        }
+        @Override
+        public Queue<Packet> makeQueue() {
+            if (queue == QueueType.SPSC)
+                return new OneToOneConcurrentArrayQueue<>(1 << POWER_OF_TWO);
+            else if (queue == QueueType.MPSC)
+                return new ManyToOneConcurrentArrayQueue<>(1 << POWER_OF_TWO);
+            else if (queue == QueueType.MPMC)
+                return new ManyToManyConcurrentArrayQueue<>(1 << POWER_OF_TWO);
+            else
+                throw new RuntimeException("no queue #: "+queue);
+        }
+    }
+
+}
